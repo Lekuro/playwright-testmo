0cda43c0d247fc2e551f1dd56208d5e9270191f9
lekuro <lekurot@gmail.com>
2026-05-25 01:44:18 +0300

    chenged selector for logo

--- src/components/header.component.ts
+++ src/components/header.component.ts
@@ -6,7 +6,10 @@ export class HeaderComponent {
   private readonly logoutButtonLocator: Locator;
 
   public constructor(private readonly page: Page) {
-    this.logoLocator = this.page.locator('a[href="/"]').first();
+    this.logoLocator = this.page
+      .locator('header')
+      .locator('a[href="/"], a[href="https://www.motionelements.com/"], a[aria-label*="MotionElements"], a:has(img[alt*="MotionElements"])')
+      .first();
     this.profileMenuLocator = this.page
       .locator('[data-testid="user-menu"], .user-menu, [aria-label*="profile"], [aria-label*="account"]')
       .first();
