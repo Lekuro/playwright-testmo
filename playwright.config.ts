25a21f2036515d98351f2d00cdc794e1e03b2979
lekuro <lekurot@gmail.com>
2026-05-25 03:08:31 +0300

    headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    headless: false,
+    headless: true,
     viewport: { width: 1600, height: 1200 },
     // viewport: null, // використання розміру вікна браузера
     // launchOptions: {
